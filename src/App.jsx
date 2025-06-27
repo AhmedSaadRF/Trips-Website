84d055e36817818023b89db275fd95197025e46b
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-27 19:25:39 +0300

    Upload Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import './App.css'
+import Navbar from './components/Navbar/Navbar.jsx'
+import Home from './components/Home/Home.jsx'
+import Middle from './components/Middle/Middle.jsx'
+import Destinations from './components/Destinations/Destinations.jsx'
+import Portifolio from './components/Portifolio/Portifolio.jsx'
+import Reviews from './components/Reviews/Reviews.jsx'
+import Questions from './components/Questions/Questions.jsx'
+import Subscribe from './components/Subscribe/Subscribe.jsx'
+import Footer from './components/Footer/Footer.jsx'
 function App() {
   return (
     <div>
-
+      <Navbar />
+      {/* <Home />
+      <Middle /> 
+      <Destinations />
+      <Portifolio />
+      <Reviews />
+      <Questions />
+      <Subscribe />
+      <Footer /> */}
     </div>
   )
 }

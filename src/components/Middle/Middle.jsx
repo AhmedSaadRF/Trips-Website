1138d44160a84a5fb3631d6548677331775051bd
AhmedSaadRF <ahmedsaadalrefaey@gmail.com>
2025-06-29 18:13:11 +0300

    Upload Project

--- src/components/Middle/Middle.jsx
+++ src/components/Middle/Middle.jsx
@@ -3,8 +3,30 @@ import "./Middle.css"
 
 export default function Middle() {
   return (
-    <div>
-      Middle
+    <div className='middle section'>
+      <div className='secContainer container'>
+        <div className="grid">
+          <span className="flex">
+            <h1>10</h1>
+            <p>World Of Experiences</p>
+          </span>
+
+          <span className="flex">
+            <h1>2K+</h1>
+            <p>Fine Destinations</p>
+          </span>
+
+          <span className="flex">
+            <h1>10K+</h1>
+            <p>Customer Reviewss</p>
+          </span>
+
+          <span className="flex">
+            <h1>4.8</h1>
+            <p>Overall Rating</p>
+          </span>
+        </div>
+      </div>
     </div>
   )
 }

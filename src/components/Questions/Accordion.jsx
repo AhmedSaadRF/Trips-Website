d78942579dda68d096ae5756dc1af78ade60fe62
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-30 17:08:20 +0300

    Upload Project

--- src/components/Questions/Accordion.jsx
+++ src/components/Questions/Accordion.jsx
@@ -1,1 +1,25 @@
+import React from 'react'
+import "./Questions.css"
+import { BsArrowDownCircle, BsArrowUpCircle } from 'react-icons/bs'
+
+function Accordion({ title, desc, active, setActive }) {
+  return (
+    <div className='accordionContainer'>
+      <span className={(active === title ? ' activeTitle ' : '') + " title" + " flex"}>
+        {title}
+        <span onClick={() => setActive(title)}>
+          {active === title ? (
+            <BsArrowUpCircle className='icon' />
+          ) : (
+            <BsArrowDownCircle className='icon' />
+          )}
+        </span>
+      </span>
+      <p className={(active === title ? ' show ' : '') + " description"}>{desc}</p>
+    </div>
+  )
+}
+
+export default Accordion
+
 

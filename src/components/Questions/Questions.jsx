b08dd51910a1904fba2e2a18a1f5d028851f5abe
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-30 17:09:14 +0300

    Upload Project

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Questions.css"
+import Accordion from './Accordion'
 
 export default function Questions() {
+  const [active, setActive] = useState("How do I choose the right travel destination for me?");
   return (
     <div className='questions section container'>
       <div className="secHeading">
@@ -9,7 +11,30 @@ export default function Questions() {
       </div>
       <div className="secContainer grid">
         <div className="accordion grid">
-
+          <Accordion 
+            title="How do I choose the right travel destination for me?" 
+            desc="description'>Consider your interests, budget, desired experiences, and the type of environment you enjoy. Research destinations that align with your preferences and offer attractions or activities you find appealing."
+            active={active}
+            setActive={setActive}
+          />
+          <Accordion 
+            title="What are the best times to visit specific destinations?" 
+            desc="Research the climate, weather patterns, and peak tourist seasons of the destination you&quot;re interested in. Opt for the shoulder seasons when the weather is pleasant, and crowds are fewer, if possible."
+            active={active}
+            setActive={setActive}
+          />
+          <Accordion 
+            title="How can I find budget-friendly travel options and deals?" 
+            desc="Look for travel deals, discounts on flights and accommodations, and consider using travel apps or websites that offer competitive prices. Being flexible with your travel dates can also help you find better deals."
+            active={active}
+            setActive={setActive}
+          />
+          <Accordion 
+            title="What essential items should I pack for my adventure?" 
+            desc="Pack appropriate clothing, toiletries, travel documents (passport, visa, etc..), essential medications, and any specific gear needed for your adventure(e.g., hiking boots, snorkeling gear)."
+            active={active}
+            setActive={setActive}
+          />
         </div>
         <div className="form">
           <div className="secHeading">

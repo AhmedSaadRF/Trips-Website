81562e7e42d8358976df67aab0a94435e9aaa64c
AhmedSaadRF <ahmedsaadalrefaey@gmail.com>
2025-07-10 08:40:53 +0300

    Upload Project

--- src/components/Destinations/Destinations.jsx
+++ src/components/Destinations/Destinations.jsx
@@ -76,7 +76,7 @@ export default function Destinations() {
   return (
     <div className='destination section container'>
       <div className="secContainer">
-        <div className="">
+        <div className="secTitle">
           <span className="redText">EXPLORE NOW</span>
           <h3>Find Your Dream Destination</h3>
           <p>Fill in the fields below to find the best spot for your next tour.</p>

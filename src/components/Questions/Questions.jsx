e9f91e75e487e23da24e45a93525498915e0dfec
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-07-11 08:55:53 +0300

    Upload Project

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -11,32 +11,32 @@ export default function Questions() {
 
   const [active, setActive] = useState("How do I choose the right travel destination for me?");
   return (
-    <div className='questions section container'> 
+    <div className='questions section container'>
       <div className="secHeading">
         <h3>Frequently Asked Questions</h3>
       </div>
       <div className="secContainer5 grid">
         <div className="accordion grid">
-          <Accordion 
-            title="How do I choose the right travel destination for me?" 
+          <Accordion
+            title="How do I choose the right travel destination for me?"
             desc="description'>Consider your interests, budget, desired experiences, and the type of environment you enjoy. Research destinations that align with your preferences and offer attractions or activities you find appealing."
             active={active}
             setActive={setActive}
           />
-          <Accordion 
-            title="What are the best times to visit specific destinations?" 
+          <Accordion
+            title="What are the best times to visit specific destinations?"
             desc="Research the climate, weather patterns, and peak tourist seasons of the destination you&quot;re interested in. Opt for the shoulder seasons when the weather is pleasant, and crowds are fewer, if possible."
             active={active}
             setActive={setActive}
           />
-          <Accordion 
-            title="How can I find budget-friendly travel options and deals?" 
+          <Accordion
+            title="How can I find budget-friendly travel options and deals?"
             desc="Look for travel deals, discounts on flights and accommodations, and consider using travel apps or websites that offer competitive prices. Being flexible with your travel dates can also help you find better deals."
             active={active}
             setActive={setActive}
           />
-          <Accordion 
-            title="What essential items should I pack for my adventure?" 
+          <Accordion
+            title="What essential items should I pack for my adventure?"
             desc="Pack appropriate clothing, toiletries, travel documents (passport, visa, etc..), essential medications, and any specific gear needed for your adventure(e.g., hiking boots, snorkeling gear)."
             active={active}
             setActive={setActive}
@@ -50,12 +50,15 @@ export default function Questions() {
           <div className="formContent grid">
             <input type="email" placeholder='Enter email address' data-aos="fade-up" />
             <textarea placeholder='Enter your quetion here' data-aos="fade-up"></textarea>
-            <button className='btn' data-aos="fade-up">Submit Inquiry</button>
+            <div className="btnx" data-aos="fade-up">
+              <button className='btn btn4'>Submit Inquiry</button>
+            </div>
           </div>
         </div>
       </div>
     </div>
   )
 }
+
 
 

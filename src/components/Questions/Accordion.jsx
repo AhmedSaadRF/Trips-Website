77ecfe049021e8ed9b05bd6e9741a01943d87d8c
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-07-11 05:30:34 +0300

    Upload Project

--- src/components/Questions/Accordion.jsx
+++ src/components/Questions/Accordion.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import "./Questions.css"
 import { BsArrowDownCircle, BsArrowUpCircle } from 'react-icons/bs'
 
 function Accordion({ title, desc, active, setActive }) {
+  useEffect(() => {
+    AOS.init({ duration: 2000 })
+  }, [])
   return (
-    <div className='accordionContainer'>
+    <div className='accordionContainer' data-aos="fade-up">
       <span className={(active === title ? ' activeTitle ' : '') + " title" + " flex"}>
         {title}
         <span onClick={() => setActive(title)}>
-          {active === title ? (
+          {active !== title ? ( 
             <BsArrowUpCircle className='icon' />
           ) : (
             <BsArrowDownCircle className='icon' />
